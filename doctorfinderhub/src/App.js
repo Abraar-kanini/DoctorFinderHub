17e65c1a24f9aeda77e5f6ee2a985b034ac41872
Abraar-kanini <abraar.kanini@gmail.com>
2024-01-30 19:43:40 +0530

    created the react files

---
+++ doctorfinderhub/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+     <div className="text-blue-600 text-center">hii this is abraar</div>
+      
+    </>
+  );
+}
+
+export default App;
