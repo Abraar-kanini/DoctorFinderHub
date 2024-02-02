bb46833d6d38e588237f1a09f782cbbb57474a55
Abraar-kanini <abraar.kanini@gmail.com>
2024-02-02 12:56:01 +0530

    created doctor section

--- doctorfinderhub/src/LandingComponent/Cards.js
+++ doctorfinderhub/src/LandingComponent/Cards.js
@@ -1,17 +1,24 @@
 import React from 'react'
 
-export default function Cards() {
+export default function Cards(props) {
   return (
     <>
-   <div className="h-[50vh] w-[20vw]  border border-black">
-   <div class="border border-black w-full h-[50%] flex justify-center items-center">
-    <img src="DoctorCard1.jpg" alt="DoctorCard" class="w-full h-full object-cover" />
-</div>
+     
+      <div className="h-[60vh] w-[20vw]  border border-white">
+        <div class="border border-white w-full h-[60%] flex justify-center items-center">
+          <img src={props.doctorimage} alt="DoctorCard" class="w-full h-full object-cover" />
+        </div>
+        <div className="space-x-3 space-y-2">
+          {/* eslint-disable jsx-a11y/heading-has-content */}
+          <h1></h1>
+          <h1 className="text-[#01579b]">{props.doctorName}</h1>
+          <h6 className="text-[#aaa]">{props.domain}</h6>
+          <p>{props.about}</p>
+        </div>
 
-       
 
-   </div>
-    
+      </div>
+
     </>
   )
 }
---
+++ doctorfinderhub/src/LandingComponent/DoctorSection.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function DoctorSection() {
+  return (
+    <>
+    
+    <div className=" w-[50vw] h-[20vh] m-auto flex flex-col justify-center items-center mt-3 space-y-4">
+          
+
+          <h1 className="font-bold text-xl ">OUR DOCTORS</h1>
+          <p className="text-[#aaa]">Our doctors are specialized in their field and have more than 10 years of experiences.</p>
+
+          <div className="w-[10vw] border border-red-500"></div>
+    </div>
+    
+    
+    
+    </>
+  )
+}
